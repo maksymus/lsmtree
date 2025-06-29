a583a8d5fd14b1bbbfe2146d8fab0cfd1b7a445d
maksymus <maksymus@gmail.com>
2025-06-28 20:48:16 -0400

    add unit tests for BloomFilter and enhance LSMTree documentation

---
+++ filter_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestBloomFilter_Add_Contains(t *testing.T) {
+	bloomFilter := NewBloomFilter(100, 0.01)
+
+	bloomFilter.Add([]byte("test"))
+	bloomFilter.Add([]byte("hello"))
+	bloomFilter.Add([]byte("world"))
+
+	if !bloomFilter.Contains([]byte("test")) {
+		t.Errorf("Expected bloom filter to contain 'test'")
+	}
+
+	if !bloomFilter.Contains([]byte("hello")) {
+		t.Errorf("Expected bloom filter to contain 'hello'")
+	}
+
+	if !bloomFilter.Contains([]byte("world")) {
+		t.Errorf("Expected bloom filter to contain 'world'")
+	}
+
+	if bloomFilter.Contains([]byte("not_in_filter")) {
+		t.Errorf("Expected bloom filter to NOT contain 'not_in_filter'")
+	}
+}
--- tree.go
+++ tree.go
@@ -1,5 +1,16 @@
 package main
 
+// LSMTree is a simple implementation of a Log-Structured Merge Tree (LSM Tree).
+// It uses an in-memory table (memTable) and a list of SSTables (sorted string tables).
+// The memTable is flushed to disk when it reaches a certain size, and the SSTables are merged periodically.
+// 			 |----------------------------------------------|
+// Memory    |            		MemTable         			|
+//           |----------------------------------------------|
+//           |   W  |   (Level 1) SStables 				    |
+// Disk      |   A  |   (Level 2) SStables    				|
+//           |   L  |   (Level 3) SStables  			    |
+// 		 	 |----------------------------------------------|
+
 func NewLSMTree() *LSMTree {
 	return &LSMTree{
 		memTable: make(map[string]string),
